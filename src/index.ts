de236fa4e8a460990376594d3bc06cdd86894852
u0105632 <fabian.winkelmann@wago.com>
2023-02-24 18:15:29 +0100

    Add dockerfile

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-const add = (a, b) => (a+b)
+'use strict';
 
-export {add}
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
