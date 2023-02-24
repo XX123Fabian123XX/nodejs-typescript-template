ef53fbfa076fa76beb92c13d35ff6c2dff4457c4
u0105632 <fabian.winkelmann@wago.com>
2023-02-24 18:04:28 +0100

    Changed test

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,6 +3,6 @@ import {add} from "../src/index"
 
 describe('sum module', () => {
   test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
+    expect(add(1, 2)).toBe(5);
   });
 });
