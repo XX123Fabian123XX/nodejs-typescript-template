f6ce1b9c7c5e4962750ab83a647abb1b5d3b4ca6
u0105632 <fabian.winkelmann@wago.com>
2023-02-24 17:57:26 +0100

    Add ci script for testing

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-const express = require("express")
+const add = (a, b) => (a+b)
 
-const app = express()
-const port = 15000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+export {add}
